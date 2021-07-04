70fc59844dedfd75a511bcd9a5033e0877b83d24
James <james@zingel.co.nz>
2021-07-05 08:24:46 +1200

    Finished up classes. Ready to train v13

--- train.py
+++ train.py
@@ -2,12 +2,12 @@
 
 # Train the specified model with input Midi
 
-from keras.models import Sequential
-from keras import layers
-from keras.optimizers import RMSprop
 from time import time
 import keras
+from keras import layers
+from keras import models
 import matplotlib.pyplot as plt
+import generate
 
 
 class Gru:
@@ -16,19 +16,19 @@ class Gru:
         self.name = name
         self.model_dir = model_dir
 
-    def build(self, lookback, num_pitches):
+    def build(self, lookback, num_pitches, loss='mse'):
         # Build the model architecture
-        model = Sequential()
-        model.add(layers.LSTM(256, input_shape=(lookback, num_pitches), return_sequences=True, dropout=0.1, recurrent_dropout=0.2))
-        model.add(layers.LSTM(128, dropout=0.1, recurrent_dropout=0.2, return_sequences=False))
+        model = models.Sequential()
+        model.add(layers.LSTM(128, input_shape=(lookback, num_pitches), return_sequences=False, dropout=0.1, recurrent_dropout=0.2))
+        #model.add(layers.LSTM(128, dropout=0.1, recurrent_dropout=0.2, return_sequences=False))
         model.add(layers.Dense(128, activation='relu'))
-        model.add(layers.Dense(64, activation='relu'))
+        #model.add(layers.Dense(64, activation='relu'))
         model.add(layers.Dense(num_pitches, activation='sigmoid'))
         model.summary()
         print("Model built.")
 
-        model.compile(loss='categorical_crossentropy',  # categorical_crossentropy or mse
-                      optimizer=RMSprop(),
+        model.compile(loss=loss,  # categorical_crossentropy or mse
+                      optimizer=keras.optimizers.RMSprop(learning_rate=1e-04),
                       metrics=['acc'])
         self.model = model
 
@@ -42,7 +42,7 @@ class Gru:
     def save(self, fname):
         save_model(self.model, fname)
 
-    def train(self, x, y, epochs, batch_size=128):
+    def train(self, x, y, epochs, batch_size=128, save_every=2, generate_every=10):
         begin = time()
         historys = []
         for i in range(epochs):
@@ -54,8 +54,15 @@ class Gru:
                                 verbose=1)
             print('Took %s minutes for epoch %s. %s minutes elapsed.' % (
             ((time() - tic) / 60).__round__(2), i+1, ((time() - begin) / 60).__round__(2)))
-            self.save(self.model_dir + self.name + ".h5")
             historys.append(history)
+
+            if i % save_every == 0:
+                self.save(self.model_dir + self.name + "-epoch-" + str(i) + ".h5")
+
+            if i % generate_every == 0:
+                out = generate.generate_music(self.model, x, [0.6], length=x.shape[0]*2, noise=True)
+                generate.multi_save(out, "outputs/generated-" + self.name + "-epoch-" + str(i) + ".h5")
+
         print('Full train took %s minutes.' % ((time() - begin) / 60).__round__(2))
         return historys
 
@@ -66,16 +73,19 @@ def save_model(model, fname):
 
 
 def load_model(fname):
-    model = keras.models.load_model(fname)
+    model = models.load_model(fname)
     print('Loaded model "' + fname + '"')
     return model
 
 
-def plot_history(history):
-    acc = history.history['acc']
-    # val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    # val_loss = history.history['val_loss']
+def plot_history(historys):
+    acc = []
+    loss = []
+
+    for history in historys:
+        acc.append(history.history['acc'])
+        loss.append(history.history['loss'])
+
     epochs = range(1, len(acc) + 1)
 
     plt.plot(epochs, acc, 'bo', label='Training acc')
